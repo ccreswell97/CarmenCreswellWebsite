db960adfa3c6dbb8cb0b570b294bab1c65a88dd5
Carmen Creswell <carmencreswell@gmail.com>
2021-04-28 23:45:28 -0500

    Add another card to Education

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -2,28 +2,43 @@ export function Education() {
     return (
         <div className="container pt-4 pl-0">
             <div className="container pl-3">
-            <h1>Education</h1>
-                <div className="card-deck pt-4">
-                    <div className="card bg-white shadow">
-                        <div className="card-body text-center">
-                            <h4 className="text-black">Bachelor's of Science, Computer Science</h4>
-                            <h6 className="text-black">Missouri State University</h6>    
-                            <ul className="card-text text-black text-left pt-2">
-                                <li>4.0 GPA</li>
-                                <li>Started the Women in Technology Club</li>
-                                <li>Received honorable mention for the Womxn of Distinction Award</li>
-                                <li>Minored in Cybersecurity</li>
-                                <li>Received a Certificate in Web Design</li>
-                                <li>Graduated Summa Cum Laude</li>
-                            </ul>
-                            <a href="https://blogs.missouristate.edu/cnas-newswatch/2018/07/05/working-for-women-in-stem/" class="card-link">Article about Womxn of Distinction Award</a>
-                            <a href="https://blogs.missouristate.edu/cnas-newswatch/2018/07/07/springfield-expedia-director-speaks-on-campus/" class="card-link">Article about WIT Club</a>
-                            <a href="https://blogs.missouristate.edu/computerscience/2017/12/18/women-in-computing-conference/" className="card-link">Article about my friends and I before we started WIT</a>
+                <h1>Education</h1>
+                    <div className="pt-4">
+                        <div className="card bg-white shadow">
+                            <div className="card-body text-center">
+                                <h4 className="text-black">Bachelor's of Science, Computer Science</h4>
+                                <h6 className="text-black">Missouri State University</h6>    
+                                <ul className="card-text text-black text-left pt-2">
+                                    <li>4.0 GPA</li>
+                                    <li>Started the Women in Technology Club</li>
+                                    <li>Received honorable mention for the Womxn of Distinction Award</li>
+                                    <li>Minored in Cybersecurity</li>
+                                    <li>Received a Certificate in Web Design</li>
+                                    <li>Graduated Summa Cum Laude</li>
+                                </ul>
+                                <a target="_blank" href="https://blogs.missouristate.edu/cnas-newswatch/2018/07/05/working-for-women-in-stem/" class="card-link">Article about Womxn of Distinction Award</a>
+                                <a target="_blank" href="https://blogs.missouristate.edu/cnas-newswatch/2018/07/07/springfield-expedia-director-speaks-on-campus/" class="card-link">Article about WIT Club</a>
+                                <a target="_blank" href="https://blogs.missouristate.edu/computerscience/2017/12/18/women-in-computing-conference/" className="card-link">Article about my friends and I before we started WIT</a>
+                            </div>
                         </div>
                     </div>
 
+                    <div className="pt-4">
+                        <div className="card bg-white shadow">
+                            <div className="card-body text-center">
+                                <h4 className="text-black">Associate of Arts, General Studies</h4>
+                                <h6 className="text-black">Crowder College</h6>    
+                                <ul className="card-text text-black text-left pt-2">
+                                    <li>4.0 GPA</li>
+                                    <li>On the Dean's List every semester</li>
+                                    <li>Finished in high school through Dual Enrollment (first one at my school to do it)</li>
+                                    <li>Graduated Summa Cum Laude</li>
+                                </ul>
+                                <a target="_blank" href="https://www.fourstateshomepage.com/news/brads-beat-high-school-student-graduates-with-college-degree/?fbclid=IwAR0oCnXSp2JRVrIlkjG2-PP2AKDEEcieiepAj_uTdUhIjU1rbOKpRik2i4E" className="card-link">Article on me graduating with my Associate's while in high school</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     );
 }

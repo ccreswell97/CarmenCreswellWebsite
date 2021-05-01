8407fe865226e727d8b86e644c4f1faacede54b0
Carmen Creswell <carmencreswell@gmail.com>
2021-05-01 14:35:27 -0500

    Add express and axios and make the api call to ravelry

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '../.env')});
+
+const express = require('express');
+const app = express();
+const port = 3001;
+const axios = require('axios');
+
+const token = Buffer.from(`${process.env.API_USERNAME}:${process.env.API_PW}`, 'utf8').toString('base64')
+
+app.get('/getHobbies', (req, res)=> {
+    axios.get('https://api.ravelry.com/projects/jimetheslime/list.json', {
+        withCredentials: true,
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            'Authorization': `Basic ${token}`
+        },
+    })
+    .then((response)=>{
+        res.json(response.data)
+    })
+    .catch(err => console.log(err));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/pages/Hobbies.jsx
+++ src/pages/Hobbies.jsx
@@ -1,5 +1,10 @@
 import {Container} from 'react-bootstrap';
 
+const apiAuth = {
+    "username": process.env.REACT_APP_API_USERNAME
+}
+
+console.log("username", apiAuth);
 export function Hobbies() {
     return (
         <Container className="pt-4 pl-0">

fb62b31e30ace248a651263cfc8bdc05a3dbf75d
Carmen Creswell <carmencreswell@gmail.com>
2023-03-20 20:30:23 -0500

    Update projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -36,31 +36,6 @@ export function Projects() {
                             Techologies used: HTML, CSS/SCSS, Netlify, Bootstrap, MailChimp
                         </Card.Footer>
                     </Card>
-
-                    <Card className="bg-white shadow" border="primary">
-                        <Card.Body className="pl-4">
-                            <Card.Title className="">Ozark Energy Services Website</Card.Title>
-                            <Card.Text>
-                                Next up on my personal project list! OES is my dad's electrical and solar company that he's had ever since I can remember. He mentioned that I should make them a website when I had just started at university, but I didn't have time. The website he has now is fine, but I'd like to build him a new, updated version.
-                            </Card.Text> 
-                        </Card.Body>
-                        <Card.Footer>
-                            Techologies to be used: React, JavaScript, CSS/SCSS, Webpack
-                        </Card.Footer>
-                    </Card>
-
-                    <Card className="bg-white shadow" border="primary">
-                        <Card.Body className="pl-4">
-                            <Card.Title className="">Board Game Selector</Card.Title>
-                            <Card.Text>
-                                I am excited to start this one! Have you ever wanted to play a board game but couldn't decide which one? Use this (future) website and enter in some information (number of players, length of game, etc.) and we will tell you what to play! This small website idea has lots of room to expand, with features like account creation and your own library of games so you can get results from your own collection.
-                            </Card.Text> 
-                        </Card.Body>
-                        <Card.Footer>
-                            Techologies to be used: React, JavaScript, CSS/SCSS, Webpack, undecided backend - possibly SpringBoot and MySQL or AWS.
-                        </Card.Footer>
-                    </Card>
-
                 </CardDeck>
             </Container>
         </Container>
